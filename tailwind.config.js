b9d5fd2b1bfca179312630a37f67eb011bf5059d
Nicolas Ortiz <161782229+NicolasDevelops@users.noreply.github.com>
2025-01-31 18:27:52 -0500

    Blackjack Game App Version 1 complete.

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#DBE9E4',
+        gradientBlue: '#B0D6CD',
+        gradientYellow: '#EFC68D',
+        gradientPurple: '#CAAFAD',
+        mainBlue: '#AEDCE2',
+        mainYellow: '#F7D6A6',
+        mainPurple: '#D6BAC3',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'cursive'],
+      },
+    },
+  },
+  plugins: [require('tailwindcss-motion')],
+}
+
